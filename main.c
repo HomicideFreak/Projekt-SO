6c177d7fd09656c430b621bd841d57ee5d3d6d71
HomicideFreak <140084516+HomicideFreak@users.noreply.github.com>
2024-01-03 23:40:27 +0100

    Switch case added. Fully functional. No signals

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include <fcntl.h>
 #include <sys/shm.h>
 #include <sys/ipc.h>
+#include <sys/time.h>
 
 #define BUFFER_MAX_LENGTH 1000
 
@@ -21,7 +22,7 @@ struct LinkedList {
     struct Node* head;
 };
 
-struct Flags 
+struct Flags
 {
     int exitFlag1;
     int exitFlag2;
@@ -86,13 +87,13 @@ int countChars(char line[BUFFER_MAX_LENGTH]){
     return (counter - 1);
 }
 
-void getLines(struct LinkedList *list, pid_t process2, int pipep1p2[2], sem_t *semP1, sem_t *semP2) {
+void getLines(struct LinkedList *list, pid_t process2, int pipep1p2[2], sem_t *semP1, sem_t *semP2, const char* fileName) {
     //printf("Start of getline()\n");
     FILE *file;
     close(pipep1p2[0]);
 
     char line[1000];
-    file = fopen("we.txt", "r");
+    file = fopen(fileName, "r");
     if (file == NULL) {
         perror("Error opening file");
         return;
@@ -127,165 +128,230 @@ void getLines(struct LinkedList *list, pid_t process2, int pipep1p2[2], sem_t *s
     fclose(file);
 }
 
+struct ProcessTimes {
+    double timeP1;
+    double timeP2;
+    double timeP3;
+};
 
 int main() {
-    struct LinkedList linkedList;
-    linkedList.head = NULL;
-    pid_t process1, process2, process3;
-    sem_t *semP1 = sem_open("semP1", O_CREAT, 0666, 0);
-    sem_t *semP2 = sem_open("semP2", O_CREAT, 0666, 0);
-    sem_t *semP3 = sem_open("semP3", O_CREAT, 0666, 0);
-    key_t memKey = 1234;
-    key_t flagMemKey = 5678;
-    int shmid = shmget(memKey, BUFFER_MAX_LENGTH, IPC_CREAT | 0666);
-    int shmid2 = shmget(flagMemKey, sizeof(struct Flags), IPC_CREAT | 0666);
-    struct Flags *exitFlags = (struct Flags*)shmat(shmid2, NULL, 0);
-    if (exitFlags == (void*)(-1))
-    {
-        perror("Flags shmat error");
-        return 1;
-    }
-    exitFlags->exitFlag1 = 0;
-    exitFlags->exitFlag2 = 0;
+    struct timeval start, end;
 
+    int menuOption, result;
+    char fileName[BUFFER_MAX_LENGTH];
+    FILE *file;
 
-    int pipep1p2[2];
-    if (pipe(pipep1p2) == -1) {
-        perror("Błąd tworzenia potoku p1-p2");
-        return 1;
-    }
-    if (shmid == -1)
-    {
-        perror("Błąd tworzenia pamięci dzielonej");
-        return 1;
-    }
-    if (shmid2 == -1)
-    {
-        perror("Błąd tworzenia pamięci dzielonej 2");
-        return 1;
-    }
-    
-    process1 = fork();
-    if (process1 == -1) {
-        perror("Błąd tworzenia procesu 1");
-        return 1;
-    } else if (process1 == 0) {
-        struct Flags *exitFlags = (struct Flags*)shmat(shmid2, NULL, 0);
-        if (exitFlags == (void*)(-1))
-        {
-            perror("Flags shmat error");
-            return 1;
+    do {
+        printf("Menu:\n");
+        printf("1. Podaj nazwę pliku do przetworzenia\n");
+        printf("2. Zakończ działanie programu\n");
+        printf("Wybierz opcję: ");
+        if (scanf("%d", &menuOption) != 1) {
+            printf("Błąd: Wprowadzono nieprawidłowe dane. Proszę użyć liczby całkowitej.\n");
+
+            // Czyszczenie bufora wejściowego
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF) { }
+
+            continue; // Powrót do początku pętli
         }
-        getLines(&linkedList, process2, pipep1p2, semP1, semP2);
-        //printf("Przed ustawieniem flagi i przed semp2 wait w proces1\n");
-        exitFlags->exitFlag1 = 1;
-        //printf("Flaga ustawiona koniec 1\n");
-        sem_post(semP2);
-        //printf("Flaga ustawiona sem2 post proces1\n");
-        shmdt(exitFlags);
-        exit(EXIT_SUCCESS);
-    } else {
-        process2 = fork();
-        if (process2 == -1) {
-            perror("Błąd tworzenia procesu 2");
-            return 1;
-        } else if (process2 == 0) {
-            //printf("Process 2 beginning\n");
-            struct Flags *exitFlags = (struct Flags*)shmat(shmid2, NULL, 0);
-            if (exitFlags == (void*)(-1))
-            {
-                perror("Flags shmat error");
-                return 1;
-            }
-            //printf("Before first wait in process2\n");
-            sem_wait(semP2);
-            close(pipep1p2[1]);
-            char buffer[BUFFER_MAX_LENGTH];
-            while (read(pipep1p2[0], buffer, BUFFER_MAX_LENGTH) > 0) {
-                int numberOfCharsInLine = countChars(buffer);
-                char message[BUFFER_MAX_LENGTH];
-                snprintf(message, sizeof(message), "Otrzymano: %s Ilość znaków: %d\n", buffer, numberOfCharsInLine);
-                char *sharedMessage = (char *)shmat(shmid, NULL, 0);
-                if (sharedMessage == (char *)(-1))
-                {
-                    perror("Błąd pamięci dzielonej - pisanie");
-                    return 1;
+
+        switch (menuOption) {
+            case 1:
+                printf("Podaj nazwę pliku: ");
+                scanf("%s", fileName);
+
+                file = fopen(fileName, "r");
+                if (file == NULL) {
+                    perror("Wystąpił błąd. Nie można otworzyć pliku");
+                    break; // Wyjście z case, powrót do menu
                 }
-                strcpy(sharedMessage, message);
-                shmdt(sharedMessage);
-                //printf("Before post sem3\n");
-                if(exitFlags->exitFlag1 == 1){
-                    exitFlags->exitFlag2 = 1;
-                    sem_post(semP3);
-                    shmdt(exitFlags);
-                    break;
-                }
-                //printf("exitFlag1: %d\n", exitFlags->exitFlag1);
-                sem_post(semP3);
-                //printf("Before wait sem2\n");
-                sem_wait(semP2);
-                
-            }
-            close(pipep1p2[0]);
-            exit(EXIT_SUCCESS);
-        } else {
-            process3 = fork();
-            if (process3 == -1)
-            {
-                perror("Błąd tworzenia procesu 3");
-                return 1;
-            }
-            else if(process3 == 0){
-                //printf("Beginning of process3\n");
+                fclose(file);
+
+                gettimeofday(&start, NULL);  // Rozpoczęcie pomiaru czasu
+                struct LinkedList linkedList;
+                linkedList.head = NULL;
+                pid_t process1, process2, process3;
+                sem_t *semP1 = sem_open("semP1", O_CREAT, 0666, 0);
+                sem_t *semP2 = sem_open("semP2", O_CREAT, 0666, 0);
+                sem_t *semP3 = sem_open("semP3", O_CREAT, 0666, 0);
+                key_t memKey = 1234;
+                key_t flagMemKey = 5678;
+                int shmid = shmget(memKey, BUFFER_MAX_LENGTH, IPC_CREAT | 0666);
+                int shmid2 = shmget(flagMemKey, sizeof(struct Flags), IPC_CREAT | 0666);
                 struct Flags *exitFlags = (struct Flags*)shmat(shmid2, NULL, 0);
                 if (exitFlags == (void*)(-1))
                 {
                     perror("Flags shmat error");
                     return 1;
                 }
-                sem_wait(semP3);
-                while (1)
+                exitFlags->exitFlag1 = 0;
+                exitFlags->exitFlag2 = 0;
+
+
+                int pipep1p2[2];
+                if (pipe(pipep1p2) == -1) {
+                    perror("Błąd tworzenia potoku p1-p2");
+                    return 1;
+                }
+                if (shmid == -1)
                 {
-                    char *sharedMessage = (char *)shmat(shmid, NULL, 0);
-                    if (sharedMessage == (char *)(-1))
+                    perror("Błąd tworzenia pamięci dzielonej");
+                    return 1;
+                }
+                if (shmid2 == -1)
+                {
+                    perror("Błąd tworzenia pamięci dzielonej 2");
+                    return 1;
+                }
+
+                struct ProcessTimes *times;
+                int shmIdTimes = shmget(IPC_PRIVATE, sizeof(struct ProcessTimes), IPC_CREAT | 0666);
+                times = (struct ProcessTimes *)shmat(shmIdTimes, NULL, 0);
+                if (times == (void *)(-1)) {
+                    perror("Błąd przyłączania segmentu pamięci dla czasów");
+                    return 1;
+                }
+
+                process1 = fork();
+
+                if (process1 == -1) {
+                    perror("Błąd tworzenia procesu 1");
+                    return 1;
+                } else if (process1 == 0) {
+                    struct Flags *exitFlags = (struct Flags*)shmat(shmid2, NULL, 0);
+                    if (exitFlags == (void*)(-1))
                     {
-                        perror("Błąd pamięci dzielonej - czytanie");
+                        perror("Flags shmat error");
                         return 1;
                     }
-                    printf("%s\n", sharedMessage);
-                    shmdt(sharedMessage);
-                    if (exitFlags->exitFlag1 == 1 && exitFlags->exitFlag2 == 1)
-                    {
-                        shmdt(exitFlags);
-                        break;
-                    }
-                    
-                    //printf("Before semp1 post\n");
-                    sem_post(semP1);
-                    //printf("Before semp3 wait\n");
-                    sem_wait(semP3);
-                }
-                exit(EXIT_SUCCESS);
-                
-            }
-            else{
+                    getLines(&linkedList, process2, pipep1p2, semP1, semP2, fileName);
+                    //printf("Przed ustawieniem flagi i przed semp2 wait w proces1\n");
+                    exitFlags->exitFlag1 = 1;
+                    //printf("Flaga ustawiona koniec 1\n");
+                    sem_post(semP2);
+                    //printf("Flaga ustawiona sem2 post proces1\n");
+                    shmdt(exitFlags);
+                    exit(EXIT_SUCCESS);
+                } else {
+                    process2 = fork();
+                    if (process2 == -1) {
+                        perror("Błąd tworzenia procesu 2");
+                        return 1;
+                    } else if (process2 == 0) {
+                        //printf("Process 2 beginning\n");
+                        struct Flags *exitFlags = (struct Flags*)shmat(shmid2, NULL, 0);
+                        if (exitFlags == (void*)(-1))
+                        {
+                            perror("Flags shmat error");
+                            return 1;
+                        }
+                        //printf("Before first wait in process2\n");
+                        sem_wait(semP2);
+                        close(pipep1p2[1]);
+                        char buffer[BUFFER_MAX_LENGTH];
+                        int counter = 1;
+                        while (read(pipep1p2[0], buffer, BUFFER_MAX_LENGTH) > 0) {
+                            int numberOfCharsInLine = countChars(buffer);
+                            char message[BUFFER_MAX_LENGTH];
+                            snprintf(message, sizeof(message), "%d. %sLiczba znaków: %d", counter, buffer, numberOfCharsInLine);
+                            char *sharedMessage = (char *)shmat(shmid, NULL, 0);
+                            if (sharedMessage == (char *)(-1))
+                            {
+                                perror("Błąd pamięci dzielonej - pisanie");
+                                return 1;
+                            }
+                            strcpy(sharedMessage, message);
+                            shmdt(sharedMessage);
+                            counter++;
+                            //printf("Before post sem3\n");
+                            if(exitFlags->exitFlag1 == 1){
+                                exitFlags->exitFlag2 = 1;
+                                sem_post(semP3);
+                                shmdt(exitFlags);
+                                break;
+                            }
+                            //printf("exitFlag1: %d\n", exitFlags->exitFlag1);
+                            sem_post(semP3);
+                            //printf("Before wait sem2\n");
+                            sem_wait(semP2);
 
-                waitpid(process1, NULL, 0);
-                waitpid(process2, NULL, 0);
-                waitpid(process3, NULL, 0);
-                close(pipep1p2[0]);
-                close(pipep1p2[1]);
-                sem_close(semP1);
-                sem_close(semP2);
-                sem_close(semP3);
-                sem_unlink("semP1");
-                sem_unlink("semP2");
-                sem_unlink("semP3");
-                shmdt(exitFlags);
-                freeLinkedList(&linkedList);
-            }
-            
+                        }
+                        close(pipep1p2[0]);
+                        exit(EXIT_SUCCESS);
+                    } else {
+                        process3 = fork();
+                        if (process3 == -1)
+                        {
+                            perror("Błąd tworzenia procesu 3");
+                            return 1;
+                        }
+                        else if(process3 == 0){
+                            //printf("Beginning of process3\n");
+                            struct Flags *exitFlags = (struct Flags*)shmat(shmid2, NULL, 0);
+                            if (exitFlags == (void*)(-1))
+                            {
+                                perror("Flags shmat error");
+                                return 1;
+                            }
+                            sem_wait(semP3);
+                            while (1)
+                            {
+                                char *sharedMessage = (char *)shmat(shmid, NULL, 0);
+                                if (sharedMessage == (char *)(-1))
+                                {
+                                    perror("Błąd pamięci dzielonej - czytanie");
+                                    return 1;
+                                }
+                                printf("%s\n", sharedMessage);
+                                shmdt(sharedMessage);
+                                if (exitFlags->exitFlag1 == 1 && exitFlags->exitFlag2 == 1)
+                                {
+                                    shmdt(exitFlags);
+                                    break;
+                                }
+
+                                //printf("Before semp1 post\n");
+                                sem_post(semP1);
+                                //printf("Before semp3 wait\n");
+                                sem_wait(semP3);
+                            }
+                            exit(EXIT_SUCCESS);
+
+                        }
+                        else{
+
+                            waitpid(process1, NULL, 0);
+                            waitpid(process2, NULL, 0);
+                            waitpid(process3, NULL, 0);
+
+                            close(pipep1p2[0]);
+                            close(pipep1p2[1]);
+                            sem_close(semP1);
+                            sem_close(semP2);
+                            sem_close(semP3);
+                            sem_unlink("semP1");
+                            sem_unlink("semP2");
+                            sem_unlink("semP3");
+                            shmdt(exitFlags);
+                            freeLinkedList(&linkedList);
+                        }
+
+                    }
+                }
+                gettimeofday(&end, NULL); // Zakończenie pomiaru czasu
+                double time_taken = end.tv_sec - start.tv_sec + (end.tv_usec - start.tv_usec) / 1000000.0;
+                printf("Całkowity czas działania programu: %f sekund\n", time_taken);
+                break;
+            case 2:
+                printf("Zakończenie programu.\n");
+                break;
+
+            default:
+                printf("Niepoprawny wybór, spróbuj ponownie.\n");
+                break;
         }
-    }
+    } while (menuOption != 2);
+
     return 0;
 }
